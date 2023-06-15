4a6b3aaa2c1fa8d33f05948634b4b7bb4e55b497
rsadevops <134621697+rsadevops@users.noreply.github.com>
2023-06-15 16:13:12 +0300

    fix: last change

--- scripts/Update_package.sh
+++ scripts/Update_package.sh
@@ -4,7 +4,6 @@ new_version=${1}
 
 echo "new_version=$new_version" >> $GITHUB_OUTPUT
 npm --no-git-tag-version version $new_version --allow-same-version
-cat package.json
 git add package.json
 git commit -m "Bump version to $new_version"
 git push --quiet
